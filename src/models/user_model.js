8885ec2d81a550e7fb22c15c169cd91059968362
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-03-13 17:39:09 +0100

    Define the schema for the documents 'User'

---
+++ src/models/user_model.js
@@ -0,0 +1,129 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (!validator.matches(value, '(?=.{7,})')) {
+                throw new Error('Password is invalid, must be min seven characters');
+            }
+        }
+    },
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    messages: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Message'
+    }],
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+}, 
+{
+    timestamps: true
+});
+
+userSchema.virtual('my_collections', {
+    ref: 'Collection',
+    localField: '_id',
+    foreignField: 'owner'
+});
+
+userSchema.virtual('my_stuffs', {
+    ref: 'Stuff',
+    localField: '_id',
+    foreignField: 'owner'
+});
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    
+    delete userObject.password;
+    delete userObject.tokens; 
+    delete userObject.messages;
+    delete userObject.followers;
+    delete userObject.following;
+
+    userObject.followers = user.followers.length;
+    userObject.following = user.following.length;
+
+    return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+
+    return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+
+    return user;
+};
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
