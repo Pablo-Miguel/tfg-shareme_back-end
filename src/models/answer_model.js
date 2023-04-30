1444f0bb471139d4dfdd105e7be6f0753b8b016d
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-30 20:06:27 +0200

    This is the schema for the answer of the question

---
+++ src/models/answer_model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const answerSchema = new mongoose.Schema({
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    body: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    question: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'QuestionAnswersMessage',
+        required: true
+    }
+});
+
+answerSchema.methods.toJSON = function() {
+    const answer = this;
+    const answerObject = answer.toObject();
+    const from = answerObject.from;
+
+    delete answerObject.from;
+
+    answerObject.from = {
+        id: from._id,
+        name: `${from.firstName} ${from.lastName}`,
+        avatar: from.avatar
+    };
+
+    return answerObject;
+}
+
+const Answer = mongoose.model('Answer', answerSchema);
+
+module.exports = Answer;
