397e05a1a177a6be49667b8fd1f9df6af2dac6a4
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-10 21:12:05 +0200

    Now with multer can save images at the backend, and saves the path in mongoDB

---
+++ controllers/upload.js
@@ -0,0 +1,33 @@
+const multer = require('multer');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        const dir = `./assets/${req.user.nickName}-${req.user._id}/imgs/`;
+        
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        
+        cb(null, dir);
+    },
+    filename: function(req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({ 
+    storage: storage,
+    limits: {
+        fileSize: 100000000 //100MB
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'));
+        }
+
+        cb(undefined, true);
+    }
+});
+
+exports.upload = upload.single('avatar');
